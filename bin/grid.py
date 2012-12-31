2eb093dd22dab4224945979569635fa84ef4a775
chrisamico <eyeseast@gmail.com>
2012-12-30 17:23:52 -0800

    Added five-year data to build script; adjusted for new CSV layout

--- bin/grid.py
+++ bin/grid.py
@@ -30,10 +30,28 @@ import argparse
 import csv
 import json
 import sys
+import os
 
 import fiona
 import random
 from shapely.geometry import box, mapping
+
+def enum(sequence, start=0, step=1):
+    "Like built-in enumerate, but with steps"
+    n = start
+    for item in sequence:
+        yield n, item
+        n += step
+
+
+def safe_float(n, nulls=['9999', 'null', '']):
+    if n in nulls:
+        return None
+    try:
+        return float(n)
+    except ValueError:
+        return None
+
 
 def get_temps(data, lat_id, lng_id):
     """
@@ -45,7 +63,7 @@ def get_temps(data, lat_id, lng_id):
     lng_id = str(lng_id)
 
     # return a list
-    return [float(d[lng_id]) for d in data if d['lat'] == lat_id]
+    return [safe_float(d[lng_id]) for d in data if d['lat'] == lat_id]
 
 
 def make_grid():
@@ -66,7 +84,7 @@ def make_grid():
     return grid
 
 
-def main(datafile='../data/tempanomaly.csv', outfile='./grid.json', format='GeoJSON'):
+def main(annual='../data/annual.csv', fiveyear=None, outfile='./grid.json', format='GeoJSON'):
     """
     Write a shapefile with our grid
     """
@@ -74,43 +92,66 @@ def main(datafile='../data/tempanomaly.csv', outfile='./grid.json', format='GeoJ
     schema = {
         'geometry': 'Polygon', 
         'properties': {
-            'lat_id': 'int',
-            'lng_id': 'int',
-            'temps': 'str',
-            'latest': 'float'
+            'lat_id'  : 'int',
+            'lng_id'  : 'int',
+            'annual'  : 'str',
+            'fiveyear': 'str',
+            'latest'  : 'float',
         }
     }
 
-    with open(datafile) as f:
+    with open(annual) as f:
         # read this file once, store a list
         # we'll iterate this list a bunch of times
         # so it's worth storing it in memory once
-        data = list(csv.DictReader(f))
+        annual = list(csv.DictReader(f))
+
+    if fiveyear:
+        with open(fiveyear) as f:
+            fiveyear = list(csv.DictReader(f))
 
     with fiona.collection(outfile, 'w', format, schema) as c:
-        for lng_id, lng in enumerate(grid, 1):
-            for lat_id, lat in enumerate(lng, 1):
-                temps = get_temps(data, lat_id, lng_id)
+        for lng_id, lng in enum(grid, -179, 2):
+            for lat_id, lat in enum(lng, -89, 2):
+                
+                if lat is None:
+                    print "Lat is None. Skipping."
+                    print "(%s, %s): %s\n" % (lng_id, lat_id, len(temps))
+                    continue
+
+                temps = get_temps(annual, lat_id, lng_id)
+                if fiveyear:
+                    fives = get_temps(fiveyear, lat_id, lng_id)
+                else:
+                    fives = []
+                
                 c.write({
                     'properties': {
-                        'lat_id': lat_id,
-                        'lng_id': lng_id,
-                        'temps': json.dumps(temps),
-                        'latest': temps[-1]
+                        'lat_id'  : lat_id,
+                        'lng_id'  : lng_id,
+                        'annual'  : json.dumps(temps),
+                        'fiveyear': json.dumps(fives),
+                        'latest'  : temps[-1] or 9999,
                     },
-                    'geometry'  : mapping(lat)
+                    'geometry': mapping(lat)
                 })
+
 
 parser = argparse.ArgumentParser(description="""\
 A conversion script for turning GISTEMP data into usable GIS files.
 """)
 
-parser.add_argument('infile',
-    help="Path to a CSV file containing tempanomaly grid data")
+parser.add_argument('annual',
+    help="Path to a CSV file containing annual tempanomaly grid data")
+parser.add_argument('fiveyear',
+    help="Path to a CSV file containing five-year averaged tempanomaly grid data")
 parser.add_argument('outfile',
     help="Destination for output file")
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-    main(args.infile, args.outfile)
+    if sys.argv[1:]:
+        args = parser.parse_args()
+        main(args.annual, args.fiveyear, args.outfile)
+    else:
+        parser.print_help()
 
